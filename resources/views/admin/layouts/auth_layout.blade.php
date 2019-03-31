24471ff9ad05e276eb7e7e5d121f49e79109581b
Online Web Tutor <onlinewebtutorhub@gmail.com>
2019-03-31 19:16:32 +0530

    Login form set up to admin panel

---
+++ resources/views/admin/layouts/auth_layout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>@yield("title")</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="{!! asset('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{!! asset('bower_components/font-awesome/css/font-awesome.min.css') !!}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{!! asset('bower_components/Ionicons/css/ionicons.min.css') !!}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{!! asset('dist/css/AdminLTE.min.css') !!}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{!! asset('plugins/iCheck/square/blue.css') !!}">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+
+    {{-- dynamic content form --}}  
+    @section("content")
+    @show
+
+<!-- jQuery 3 -->
+<script src="{!! asset('bower_components/jquery/dist/jquery.min.js') !!}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{!! asset('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}"></script>
+<!-- iCheck -->
+<script src="{!! asset('plugins/iCheck/icheck.min.js') !!}"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
+</body>
+</html>
---
+++ resources/views/admin/views/login_form.blade.php
@@ -0,0 +1,39 @@
+@extends("admin.layouts.auth_layout")
+
+@section("title","Admin Login")
+
+@section("content")
+
+<div class="login-box">
+    <div class="login-logo">
+        <a href="javascript:void(0)"><b>Admin</b>LTE</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">Login Panel</p>
+
+        <form action=javascript:void(0)" method="post">
+            <div class="form-group has-feedback">
+                <input type="email" class="form-control" placeholder="Email">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+
+    </div>
+    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+@endsection
